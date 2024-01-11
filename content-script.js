08b9e65f1e7ea616958b6f179d5c33680896fa64
yashksaini <yashdms1@gmail.com>
2024-01-11 08:05:28 +0530

    feat: :sparkles: Chrome extension completed

--- content-script.js
+++ content-script.js
@@ -13,8 +13,8 @@ function run() {
       executeBasics();
     })
     .catch((error) => {
-      console.error("Error fetching HTML file:", error.message); // Log the error message
-      console.error("Error stack trace:", error.stack); // Log the stack trace for detailed error info
+      console.error("Error fetching HTML file:", error.message);
+      console.error("Error stack trace:", error.stack);
     });
 }
 function executeBasics() {
@@ -46,49 +46,66 @@ function executeBasics() {
   });
 
   function displayList() {
-    list.innerHTML = "";
-    arr.forEach((data, index) => {
-      const url = "/c/" + data.id;
-      const listItem = document.createElement("div");
-      listItem.classList.add("saved-link");
-      listItem.innerHTML = `<a href='${url}'>${data.name}</a><span class='pins'><svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="15"
-        height="15"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <path d="M4 7V21a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3M2 7h20M7 11V18M17 11V18" />
-      </svg></span>`;
+    if (arr.length > 0) {
+      list.innerHTML = "";
+      arr.forEach((data, index) => {
+        const url = "/c/" + data.id;
+        const listItem = document.createElement("div");
+        listItem.classList.add("saved-link");
+        listItem.innerHTML = `<a href='${url}'>${data.name}</a><span class='pins'><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
+        <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" fill="#ffffff"/>
+      </svg>
+      </span>`;
 
-      const pin = listItem.querySelector(".pins");
-      pin.addEventListener("click", function () {
-        console.log(index, "Pin Removed");
+        const pin = listItem.querySelector(".pins");
+        pin.addEventListener("click", function () {
+          arr.splice(index, 1);
+          localStorage.setItem("pinChats", JSON.stringify(arr));
+          showToast("Pinned Chat Removed");
+          displayList();
+        });
+
+        list.appendChild(listItem);
       });
-
-      list.appendChild(listItem);
-    });
+    } else {
+      list.innerHTML = `<div class="saved-link"><a>List appear here...</a></div>`;
+    }
   }
 
   save.addEventListener("click", function () {
     const url = window.location.href;
     const parts = url.split("/");
     const id = parts[parts.length - 1];
-    let data = {
-      name: document.title,
-      id: id,
-    };
-    arr.push(data);
+
+    // Check if data with the same id already exists
+    const existingDataIndex = arr.findIndex((item) => item.id === id);
+
+    if (existingDataIndex !== -1) {
+      arr[existingDataIndex].name = document.title;
+      showToast("Pinned title updated");
+    } else {
+      let data = {
+        name: document.title,
+        id: id,
+      };
+      arr.push(data);
+      showToast("Chat Pinned");
+    }
     localStorage.setItem("pinChats", JSON.stringify(arr));
     list.innerHTML = "";
-    console.log(arr);
     displayList();
-    alert("Chat Saved");
   });
+  function showToast(message) {
+    const toast = document.getElementById("toast");
+    toast.innerHTML = message;
+    toast.style.display = "block";
+
+    // Hide the toast after 2 seconds
+    setTimeout(function () {
+      toast.style.display = "none";
+    }, 2000);
+  }
+
   displayList();
 }
 
